7b12352da54380556a4e6ee97adc8897cf905c16
RolandYambao <rolandyambao@gmail.com>
2021-12-17 14:35:08 -0800

    Updated

--- src/Article.js
+++ src/Article.js
@@ -13,21 +13,21 @@ class Article extends Component {
                     <div className="tile is-parent is-shady">
                         <article className="tile is-child notification is-white">
                             <p className="title">{this.props.title1}</p>
-                            <p className="subtitle">What is up?</p>
+                            <p className="subtitle">{this.props.content1}</p>
                         </article>
                     </div>
                     <div className="tile is-parent is-shady">
                         <article className="tile is-child notification is-white">
-                            <p className="title">Foo</p>
-                            <p className="subtitle">Bar</p>
+                            <p className="title">{this.props.title2}</p>
+                            <p className="subtitle">{this.props.content2}</p>
                         </article>
                     </div>
                     <div className="tile is-parent is-shady">
                         <article className="tile is-child notification is-white">
-                            <p className="title">Third column</p>
+                            <p className="title">{this.props.title3}</p>
                             <p className="subtitle">With some content</p>
                             <div className="content">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+                                <p>{this.props.content3}</p>
                             </div>
                         </article>
                     </div>
@@ -37,18 +37,18 @@ class Article extends Component {
                         <div className="tile">
                             <div className="tile is-parent is-vertical">
                                 <article className="tile is-child notification is-white">
-                                    <p className="title">Vertical tiles</p>
-                                    <p className="subtitle">Top box</p>
+                                    <p className="title">{this.props.title4}</p>
+                                    <p className="subtitle">{this.props.content4}</p>
                                 </article>
                                 <article className="tile is-child notification is-white">
-                                    <p className="title">Vertical tiles</p>
-                                    <p className="subtitle">Bottom box</p>
+                                    <p className="title">{this.props.title5}</p>
+                                    <p className="subtitle">{this.props.content5}</p>
                                 </article>
                             </div>
                             <div className="tile is-parent">
                                 <article className="tile is-child notification is-white">
-                                    <p className="title">Middle box</p>
-                                    <p className="subtitle">With an image</p>
+                                    <p className="title">{this.props.title6}</p>
+                                    <p className="subtitle">{this.props.content6}</p>
                                     <figure className="image is-4by3">
                                         <img src="https://picsum.photos/640/480/?random" alt="Description" />
                                     </figure>
@@ -57,10 +57,10 @@ class Article extends Component {
                         </div>
                         <div className="tile is-parent is-shady">
                             <article className="tile is-child notification is-white">
-                                <p className="title">Wide column</p>
+                                <p className="title">{this.props.title7}</p>
                                 <p className="subtitle">Aligned with the right column</p>
                                 <div className="content">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+                                    <p>{this.props.content7}</p>
                                 </div>
                             </article>
                         </div>
@@ -68,15 +68,10 @@ class Article extends Component {
                     <div className="tile is-parent is-shady">
                         <article className="tile is-child notification is-white">
                             <div className="content">
-                                <p className="title">Tall column</p>
+                                <p className="title">{this.props.title8}</p>
                                 <p className="subtitle">With even more content</p>
                                 <div className="content">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut. Morbi maximus, leo sit amet vehicula
-                                        eleifend, nunc dui porta orci, quis semper odio felis ut quam.</p>
-                                    <p>Suspendisse varius ligula in molestie lacinia. Maecenas varius eget ligula a sagittis. Pellentesque interdum, nisl nec interdum maximus, augue diam porttitor lorem, et sollicitudin felis neque sit amet erat. Maecenas imperdiet
-                                        felis nisi, fringilla luctus felis hendrerit sit amet. Aenean vitae gravida diam, finibus dignissim turpis. Sed eget varius ligula, at volutpat tortor.</p>
-                                    <p>Integer sollicitudin, tortor a mattis commodo, velit urna rhoncus erat, vitae congue lectus dolor consequat libero. Donec leo ligula, maximus et pellentesque sed, gravida a metus. Cras ullamcorper a nunc ac porta. Aliquam
-                                        ut aliquet lacus, quis faucibus libero. Quisque non semper leo.</p>
+                                    <p>{this.props.content8}</p>
                                 </div>
                             </div>
                         </article>
@@ -85,19 +80,19 @@ class Article extends Component {
                 <div className="tile is-ancestor">
                     <div className="tile is-parent is-shady">
                         <article className="tile is-child notification is-white">
-                            <p className="title">Side column</p>
+                            <p className="title">{this.props.title9}</p>
                             <p className="subtitle">With some content</p>
                             <div className="content">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+                                <p>{this.props.content9}</p>
                             </div>
                         </article>
                     </div>
                     <div className="tile is-parent is-8 is-shady">
                         <article className="tile is-child notification is-white">
-                            <p className="title">Main column</p>
+                            <p className="title">{this.props.title10}</p>
                             <p className="subtitle">With some content</p>
                             <div className="content">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+                                <p>{this.props.content10}</p>
                             </div>
                         </article>
                     </div>
@@ -105,19 +100,19 @@ class Article extends Component {
                 <div className="tile is-ancestor">
                     <div className="tile is-parent is-8 is-shady">
                         <article className="tile is-child notification is-white">
-                            <p className="title">Murphy's law</p>
+                            <p className="title">{this.props.title11}</p>
                             <p className="subtitle">Anything that can go wrong will go wrong</p>
                             <div className="content">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+                                <p>{this.props.content11}</p>
                             </div>
                         </article>
                     </div>
                     <div className="tile is-parent is-shady">
                         <article className="tile is-child notification is-white">
-                            <p className="title">Main column</p>
+                            <p className="title">{this.props.title12}</p>
                             <p className="subtitle">With some content</p>
                             <div className="content">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+                                <p>{this.props.content12}</p>
                             </div>
                         </article>
                     </div>

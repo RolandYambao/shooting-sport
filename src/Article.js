d2d2ca5abbf5f482058d797a2d0174c2632cd133
RolandYambao <rolandyambao@gmail.com>
2021-12-17 13:54:41 -0800

    Updated

---
+++ src/Article.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Article extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        // return (
+
+        // );
+    }
+}
+
+export default Article;
